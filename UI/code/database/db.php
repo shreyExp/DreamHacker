9f09859200b6eaed769d37dd8621b8fbe6c0a714
Michael <2529225H@student.gla.ac.uk>
2021-04-09 19:00:33 +0100

    Database setup readme file

--- UI/code/database/db.php
+++ UI/code/database/db.php
@@ -1,8 +1,8 @@
 <?php
 /* Database connection settings */
 $host = 'localhost';
-$user = 'dissertation';
-$pass = '091164383707572306304pass';
+$user = ''; /* insert your username */
+$pass = ''; /* insert your password */
 $db = 'dreamHacker';
 $mysqli = new mysqli($host,$user,$pass, $db) or die($mysqli->error);
 ?>
