98ed8ea83bdd295068eafc0fe82b74b1a98693cd
praveen1992 <praveen.nitap@gmail.com>
2021-04-16 23:22:17 +0100

    doxygen test run

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,24 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acdghjmprst~",
-  1: "cgjmprs",
-  2: "acghjpst~",
-  3: "d"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions",
-  3: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Pages"
 };
 
