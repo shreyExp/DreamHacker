633589501b7ac5d8a93948e34e7eb62559b76030
praveen.tiwari@admissiontable.com-at-846247677462 <praveen.nitap@gmail.com>
2021-04-19 16:18:54 +0100

    Added docs

---
+++ docs/annotated_dup.js
@@ -0,0 +1,13 @@
+var annotated_dup =
+[
+    [ "CppTimer", "class_cpp_timer.html", "class_cpp_timer" ],
+    [ "CppTimerCallback", "class_cpp_timer_callback.html", "class_cpp_timer_callback" ],
+    [ "JSONCGIADCCallback", "class_j_s_o_n_c_g_i_a_d_c_callback.html", "class_j_s_o_n_c_g_i_a_d_c_callback" ],
+    [ "JSONCGIHandler", "class_j_s_o_n_c_g_i_handler.html", "class_j_s_o_n_c_g_i_handler" ],
+    [ "MainWindow", "class_main_window.html", "class_main_window" ],
+    [ "QMainWindow", "class_q_main_window.html", null ],
+    [ "SenseWindow", "class_sense_window.html", "class_sense_window" ],
+    [ "SensorCallback", "class_sensor_callback.html", "class_sensor_callback" ],
+    [ "SENSORfastcgicallback", "class_s_e_n_s_o_rfastcgicallback.html", "class_s_e_n_s_o_rfastcgicallback" ],
+    [ "SensorTimer", "class_sensor_timer.html", "class_sensor_timer" ]
+];
---
+++ docs/main_8cpp.js
@@ -0,0 +1,9 @@
+var main_8cpp =
+[
+    [ "SENSORfastcgicallback", "class_s_e_n_s_o_rfastcgicallback.html", "class_s_e_n_s_o_rfastcgicallback" ],
+    [ "JSONCGIADCCallback", "class_j_s_o_n_c_g_i_a_d_c_callback.html", "class_j_s_o_n_c_g_i_a_d_c_callback" ],
+    [ "main", "main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97", null ],
+    [ "signalHandler", "main_8cpp.html#a8ee3282bc313e547dbbb8d4f4010db61", null ],
+    [ "usage", "main_8cpp.html#ae8605e2b78cd4a81b6c6b5c30cb7366a", null ],
+    [ "g_running", "main_8cpp.html#a037aed45f792fc3e78387006be43a53e", null ]
+];
